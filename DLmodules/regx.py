686a72d9cb3ec422c83df5fb0762d1dce8eb9901
egg0001 <38551393+egg0001@users.noreply.github.com>
2020-02-29 19:59:48 -0800

    HTTPS for images
    1. Downloading images from https links
    2. Updating required module list

--- DLmodules/regx.py
+++ DLmodules/regx.py
@@ -19,7 +19,7 @@ mangaPagePattern = (r'''https://[a-z\.\-]+\.org\/s\/[a-z0-9]+\/[a-z0-9]+\-([0-9]
 
 nextPagePattern = (r'''<a href="(https://[a-z\.\-]+\.org\/g/[a-z0-9]+/[a-z0-9]+/\?p\=[0-9]+)" onclick="return false">&gt;</a>''')
 
-imagePattern = ('''<img id="img" src="(http://.+)" style="''')
+imagePattern = ('''<img id="img" src="(https*://.+)" style="''')
 
 reloadPattern = (r'''id\=\"loadfail\" onclick\=\"return nl\(\'([0-9\-]+)\'\)\"''')
 
